d49b910bcd7415aa93bee239cbce2d8a1f14e9d4
tolanio24 <tolanio24@gmail.com>
2024-01-30 14:47:03 -0500

    trying to fix not getting updates

---
+++ JavaDemo/src/MainApplication.java
@@ -0,0 +1,11 @@
+public class MainApplication {
+
+    // public static void main - this is the entry point of the program
+    public static void main(String)[] args) {
+
+    System.out.println("HTML");
+    System.out.println("CSS");
+
+    String
+    }
+}
---
+++ PluralSight/src/MathApp.java
@@ -0,0 +1,17 @@
+public class MathApp {
+    public static void main(String[] args) {
+        // Question 1: create 2 vars for bob and gary salary, create new var for highest salary,
+        // greatest salary print"the highest is"
+        int bobSalary = 10000;
+        int garySalary = 20000;
+        int highestSalary = Math.max(bobSalary, garySalary);
+        System.out.println("The highest salary is " + highestSalary);
+        System.out.println("------------------");
+
+        // declare variables here
+        // then code solution
+        // then use System.out.println() to display results
+        // ex: System.out.println("The answer is " + answer);
+        // REPEAT FOR NEXT EXERCISE
+    }
+}
